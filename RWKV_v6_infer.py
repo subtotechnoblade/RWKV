16cd1e44dccfebe9ee11b202e88cf127d5d5f293
TryHarder <brian8204001@gmail.com>
2024-12-23 17:21:12 -0500

    Updated RWKV, fully working

--- RWKV_v6_infer.py
+++ RWKV_v6_infer.py
@@ -56,15 +56,11 @@ class Multi_Headed_Dense(tf.keras.layers.Layer):
         # (b, c, h, e, e)
 
         self.denses = [tf.keras.layers.Dense(self.head_dim) for _ in range(self.num_heads)]
-        # self.dense = tf.keras.layers.Dense(self.head_dim, name="grr")
 
     # @tf.function()
     def call(self, inputs):
-        # context_size = tf.shape(inputs)[1]
-        # return tf.reshape(self.dense(inputs), (-1, context_size, self.num_heads, self.head_dim))
-        inputs = tf.transpose(inputs, [2, 0, 1, 3])
-        return tf.transpose(tf.stack([self.denses[i](inputs[i]) for i in range(self.num_heads)]), [1, 2, 0, 3])
-
+        x = tf.squeeze(tf.stack([self.denses[i](inputs[i: i + 1]) for i in range(self.num_heads)]), axis=1)
+        return x
 
 class Time_Mix(tf.keras.layers.Layer):
     def __init__(self, layer_id, num_heads, embed_size, token_shift_hidden_dim=32, **kwargs):
@@ -134,8 +130,6 @@ class Time_Mix(tf.keras.layers.Layer):
         self.gn = tf.keras.layers.GroupNormalization(self.num_heads, axis=-1, name=f"GroupNorm_{self.layer_id}")
         self.out = tf.keras.layers.Dense(self.embed_size, name=f"Out_{self.layer_id}")
         # self.built = True
-    def token_shift(self, inputs):
-        return tf.pad(inputs, [[0, 0], [1, 0], [0, 0]])[:, :-1]
 
     def token_shift_v6(self, x, last_x, mu, l, A_matrix, B_matrix):
         """
@@ -153,7 +147,6 @@ class Time_Mix(tf.keras.layers.Layer):
         # output = x * mu + last_x * (1 - mu)
         # first compute (b - a) call this diff
         """
-        batch_size, context_length, embed_size = tf.shape(x)[0], tf.shape(x)[1], tf.shape(x)[2]
         diff = x - last_x
         lora = x + (diff * mu)
         lora = B_matrix(tf.nn.tanh(A_matrix(lora)))
@@ -162,60 +155,15 @@ class Time_Mix(tf.keras.layers.Layer):
         output = lora * diff
         output += x
         # # # split the output into individual heads
-        return tf.reshape(output, [batch_size, context_length, self.num_heads, self.head_dim])
-        # return tf.reshape(x, [batch_size, context_length, self.num_heads, self.head_dim])
+        return tf.reshape(output, [self.num_heads, self.head_dim])
 
-    def combine_fn(self, sum_a, prod_a, sum_b, prod_b):
-        """
-        Given arr = [(x0, 1), (x1, w0), (x2, w1)]
-        :param a: accumulated result
-        :param b: new elements from the initial tensor
-        :return:
-        """
-        # print(sum_a.shape, prod_a.shape, sum_b.shape, prod_b.shape)
-        # return sum_b + prod_a * sum_a, prod_b * prod_a
-        return sum_b + prod_a * sum_a, prod_b
 
-    # @tf.function(jit_compile=True)
-    def serial_scam(self, u, w, kv):
-        """
-        Note that context_length
-        :param u: bonus (batch, context_length, embed)
-        :param w: decay (batch, context_length, embed)
-        :param ktv: (batch, context_length, embed, embed) # the matrix update for every time step / token step
-        :return: the matrix memory at every timestep according to https://github.com/BlinkDL/RWKV-LM/blob/main/RWKV-v6.png
-        """
-        # decay (w) is [:, 1:] because in the equations w0 is simply not used and x0 goes with w1 not w0
-        matrix_mem_update, product_acc = tfp.math.scan_associative(lambda a, b: self.combine_fn(*a, *b),
-                                                                   (kv[:, :-1], tf.expand_dims(w[:, 1:], axis=-1)),
-                                                                   axis=1)
-        del product_acc
 
-        matrix_mem = kv * tf.expand_dims(u, axis=-1)
-        matrix_mem += tf.concat((tf.zeros_like(kv[:, :1], dtype=global_dtype), matrix_mem_update), axis=1)
-        return matrix_mem
 
     # @tf.function()
-    def serial_scam2(self, u, w, kv):
-        # w = tf.concat((tf.ones_like(w[:, :1]), w[:, 1:]), axis=1)
-
-        w = tf.expand_dims(w, axis=-1)
-        w_reshaped = tf.transpose(w, [1, 0, 2, 3, 4])
-        kv_reshaped = tf.transpose(kv, [1, 0, 2, 3, 4])
-        # decay (w) is [1:] because in the equations w0 is simply not used and x0 goes with w1 not w0
-        matrix_mem_update, _ = tf.scan(lambda a, b: self.combine_fn(*a, *b), (kv_reshaped[:-1], w_reshaped[1:]))
-        matrix_mem_update = tf.transpose(matrix_mem_update, [1, 0, 2, 3, 4])
-        matrix_mem = kv * tf.expand_dims(u, axis=-1)
-
-        matrix_mem += tf.concat((tf.zeros_like(kv[:, :1]), matrix_mem_update), axis=1)
-        return matrix_mem
-
-    # @tf.function()
-    def call(self, inputs, trainable=True, *args, **kwargs):
-        batch_size, context_length, embed_size = tf.shape(inputs)[0], tf.shape(inputs)[1], tf.shape(inputs)[2]
+    def call(self, inputs, state, state_matrix, trainable=True, *args, **kwargs):
         x = inputs
-        last_x = self.token_shift(inputs)
-
+        last_x = state[self.layer_id][0]
         k = self.key(self.token_shift_v6(x, last_x, self.key_mu, self.key_lambda, self.key_A, self.key_B))  # keys
 
 
@@ -236,24 +184,26 @@ class Time_Mix(tf.keras.layers.Layer):
         g = self.gate(x * self.gate_mu + last_x * (1 - self.gate_mu))  # for gating we don't have heads
 
         # k and v: batch, h, context, embed, 1
-        kv = tf.expand_dims(k, axis=-1) @ tf.expand_dims(v, axis=3)
-        w = tf.exp(-tf.exp(w))
 
-        # wkv = self.serial_scam(u, w, kv)  # <- serial scan goes here
-        # wkv1 = self.serial_scam1(u, w, ktv)  # <- serial scan goes here
-        # wkv = self.serial_scam2(1, w, kv)  # <- serial scan goes here
-        wkv = self.serial_scam2(u, w, kv)  # <- serial scan goes here
-        # print(wkv.shape)
+        kv = tf.expand_dims(k, axis=-1) @ tf.expand_dims(v, axis=1)
+
+        w = tf.expand_dims(tf.exp(-tf.exp(w)), axis=-1)
+        u = tf.expand_dims(u, axis=-1)
+
+
+        wkv = kv * u + state_matrix[self.layer_id]
+
+        state_matrix = tf.tensor_scatter_nd_update(state_matrix, tf.constant([[self.layer_id]]),
+                                                   tf.expand_dims(w * state_matrix[self.layer_id] + kv, axis=0))
+
         rwkv = tf.squeeze(wkv @ tf.expand_dims(r, axis=-1), axis=-1)
 
-        rwkv = tf.reshape(rwkv, [batch_size * context_length, embed_size])
+        rwkv = tf.reshape(rwkv, [1, self.embed_size])
         rwkv = self.gn(rwkv)
-        rwkv = tf.reshape(rwkv, [batch_size, context_length, embed_size])
+        state = tf.tensor_scatter_nd_update(state, tf.constant([[self.layer_id, 0]]), inputs)
 
-        rwkv *= tf.nn.silu(g)
-
-        time_mixed = self.out(rwkv)
-        return time_mixed
+        time_mixed = self.out(rwkv * tf.nn.silu(g))
+        return time_mixed, state, state_matrix
 
 
 class Channel_Mix(tf.keras.layers.Layer):
@@ -275,13 +225,11 @@ class Channel_Mix(tf.keras.layers.Layer):
         # self.mish = Mish()
         self.sigmoid = tf.keras.layers.Activation(tf.keras.activations.sigmoid)
 
-    def token_shift(self, inputs):
-        return tf.pad(inputs, [[0, 0], [1, 0], [0, 0]])[:, :-1]
 
     # @tf.function(jit_compile=True)
-    def call(self, inputs, trainable=True, *args, **kwargs):
+    def call(self, inputs, state, trainable=True, *args, **kwargs):
         x = inputs
-        last_x = self.token_shift(inputs)
+        last_x = state[self.layer_id][1]
 
         k = self.key(x * self.mix_k + last_x * (1 - self.mix_k))
         # k = self.mish(k)
@@ -292,7 +240,9 @@ class Channel_Mix(tf.keras.layers.Layer):
         r = self.sigmoid(r)
 
         rkv = kv * r
-        return rkv
+
+        state = tf.tensor_scatter_nd_update(state, tf.constant([[self.layer_id, 1]]), inputs)
+        return rkv, state
 
 
 class RWKV_Block(tf.keras.layers.Layer):
@@ -306,41 +256,78 @@ class RWKV_Block(tf.keras.layers.Layer):
         self.channel_mix = Channel_Mix(layer_id, embed_size, hidden_size)
 
 
-    def call(self, inputs, trainable=True, *args, **kwargs):
+    def call(self, inputs, state, state_matrix, trainable=True, *args, **kwargs):
         x = self.layer_norm1(inputs)
-        x = self.time_mix(x)
+        x, state, state_matrix = self.time_mix(x, state, state_matrix)
         x += inputs
         residual = x
         x = self.layer_norm2(x)
-        x = self.channel_mix(x)
+        x, state = self.channel_mix(x, state)
         x += residual
-        return x
-def make_test_model(embed_size, num_heads=1, num_layers=1):
-    inputs = tf.keras.layers.Input(batch_shape=(None, None, embed_size), name="inputs")
-    x = inputs
-    for layer_id in range(num_layers):
-        x = RWKV_Block(layer_id, num_heads, embed_size)(x)
+        return x, state, state_matrix
 
-
-    return  tf.keras.Model(inputs=inputs, outputs=x)
 if __name__ == "__main__":
     import numpy as np
+    from RWKV_v6 import make_test_model
+    embed_size, num_heads, num_layers = 64, 4, 5
 
-    model = make_test_model(64, 4, 1)
+
+    model = make_test_model(embed_size, num_heads, num_layers)
     model.summary()
 
-    x_train = np.random.randint(low=0, high=100, size=(4, 2, 64))
-    print(x_train.shape)
-    y_train = 2 * x_train + 1
+    # model(np.random.randint(low=0, high=100, size=(1, 2, embed_size)))
 
-    model.compile(optimizer=tf.keras.optimizers.AdamW(learning_rate=1e-2),
-                  loss="mse",
-                  metrics=["acc", "mae"])
-    model.fit(x_train, y_train, epochs=150)
-    x = np.random.randint(low=0, high=100, size=(4, 2, 64))
-    print(x[0][0])
-    print()
-    print(model(x)[0][0])
+    model.save_weights("test_model.weights.h5")
+
+
+    def make_test_model_infer(embed_size, num_heads=1, num_layers=1):
+        inputs = [tf.keras.layers.Input(batch_shape=(None, embed_size), name="inputs"),
+        tf.keras.layers.Input(batch_shape=(num_layers, 2, embed_size), name="input_state"),
+        tf.keras.layers.Input(batch_shape= (num_layers, num_heads, embed_size // num_heads, embed_size // num_heads), name="inputs_state_matrix")]
+
+        x, state, state_matrix = inputs
+
+        for layer_id in range(num_layers):
+            x, state, state_matrix = RWKV_Block(layer_id, num_heads, embed_size)(x, state, state_matrix)
+        output_state, output_state_matrix = state, state_matrix
+
+        return tf.keras.Model(inputs=inputs, outputs=[x, output_state, output_state_matrix])
+
+    model_infer = make_test_model_infer(embed_size, num_heads, num_layers)
+    # model_infer.summary()
+
+    def create_states():
+        return np.zeros((num_layers, 2, embed_size)), np.zeros(
+            (num_layers, num_heads, embed_size // num_heads, embed_size // num_heads))
+    input_state, input_state_matrix = create_states()
+
+
+    model_infer.load_weights("test_model.weights.h5")
+
+    dummy_data = np.random.uniform(low=0, high=100, size=(1, 360, embed_size))
+
+    output1 = model(dummy_data)
+    # print(output1)
+
+    output2 = []
+    for data_point in dummy_data[0]:
+        data_point = np.expand_dims(data_point, 0)
+
+        x, input_state, input_state_matrix = model_infer([data_point,input_state, input_state_matrix])
+        input_state = input_state.numpy()
+        input_state_matrix = input_state_matrix.numpy()
+        output2.append(x)
+
+    output1 = np.array(output1).reshape(-1, 64)
+    output2 = np.array(output2).reshape(-1, 64)
+    print(output1.shape, output2.shape)
+
+    index = 1
+    print(np.allclose(output1, output2))
+    for index in range(len(output1)):
+        # print(output1[index], output2[index])
+        print(np.sum(np.abs(output1[index] - output2[index])))
+
 
 
 
